86a58a313c5f8361a2aa68ef63b710a853f0a2b9
David Rodriguez Gallego <davidrodriguezgallego@gmail.com>
2025-04-08 22:59:30 +0200

    Update prueba_script.sh

--- prueba_script.sh
+++ prueba_script.sh
@@ -8,6 +8,21 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
+# Verificar si Docker está instalado
+if ! command -v docker &> /dev/null; then
+  echo "❌ Docker no está instalado. Instalándolo..."
+  curl -sSL https://get.docker.com | sh
+  sudo systemctl enable docker
+  sudo usermod -aG docker $USER
+  sudo systemctl start docker
+fi
+
+# Verificar si docker-compose está instalado
+if ! command -v docker-compose &> /dev/null; then
+  echo "❌ docker-compose no está instalado. Instalándolo..."
+  sudo apt install -y docker-compose || { echo "Error al instalar docker-compose"; exit 1; }
+fi
+
 # Instalación Adguard
 
 # 1. Crear carpetas necesarias
