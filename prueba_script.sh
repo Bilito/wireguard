e4047f860b2c47b6902db1cb2b75f8381c36e916
David Rodriguez Gallego <davidrodriguezgallego@gmail.com>
2025-04-11 16:23:32 +0200

    Update prueba_script.sh

--- prueba_script.sh
+++ prueba_script.sh
@@ -8,62 +8,13 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
-# Verificar si Docker está instalado
-if ! command -v docker &> /dev/null; then
-  echo "❌ Docker no está instalado. Instalándolo..."
-  curl -sSL https://get.docker.com | sh
-  sudo systemctl enable docker
-  sudo usermod -aG docker $USER
-  sudo systemctl start docker
-fi
-
-# Verificar si docker-compose está instalado
-if ! command -v docker-compose &> /dev/null; then
-  echo "❌ docker-compose no está instalado. Instalándolo..."
-  sudo apt install -y docker-compose || { echo "Error al instalar docker-compose"; exit 1; }
-fi
-
-# Instalación Adguard
-
-# 1. Crear carpetas necesarias
-
-mkdir ./adguard
-cd ./adguard
-mkdir config && mkdir workingdir || exit
-
-# 2. Crear docker-compose.yml
-echo "Creando docker-compose.yml para adguard..."
-cat <<EOF > docker-compose.yml
-
-
-services:
-    adguardhome:
-        container_name: adguard-home
-        environment:
-            - TZ=Europe/Madrid
-        volumes:
-            - ./config:/opt/adguardhome/conf
-            - ./workingdir:/opt/adguardhome/work
-        restart: always
-        network_mode: host
-        image: adguard/adguardhome
-EOF
-
-# 8. Ejecutar docker-compose ya con la config lista
-echo "Levantando el contenedor con la configuración final..."
-sudo docker-compose up -d
-
-echo "✅ ¡Adguard está listo! Accede a la interfaz web"
 
 read -p "Introduce el dominio DDNS o IP pública para el endpoint del servidor (ej. midominio.ddns.net): " ENDPOINT
 
-# Actualización de paquetes del sistema
-echo "Actualizando los paquetes del sistema..."
-apt update && apt upgrade -y
 
 # Instalación de WireGuard y sus herramientas
 echo "Instalando WireGuard..."
-apt install -y wireguard wireguard-tools qrencode
+apt install -y wireguard qrencode
 
 # Generar claves para el servidor
 echo "Generando claves para el servidor..."
